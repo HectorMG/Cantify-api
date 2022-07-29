8b1b0ee05cca1bcd95348e624fc320623b97fc53
HectorMG <hector980715@gmail.com>
2022-07-28 20:06:35 -0500

    Creación estructura basica del proyecto

---
+++ config/database.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    url: "mongodb+srv://hectorfmartinez:3BkfCGexOq6oikNA@cluster0.k1i9s9b.mongodb.net/cantify?retryWrites=true&w=majority",
+    options: {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    },    
+  };
+  
---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
+const dbConfig = require("./config/database.config.js");
+const mongoose = require("mongoose");
+
+const app = express();
+
+mongoose.Promise = global.Promise;
+
+// Parse requests of content-type - "application/x-www-form-urlencoded"
+app.use(bodyParser.urlencoded({ extended: true }));
+// Parse requests of content-type - "application/json"
+app.use(bodyParser.json());
+// Activate the CORS access on all routes
+app.use(cors());
+// Listening server port
+var port = process.env.PORT || 3681;
+
+// Define the routes [4]
+////////////////////////////////////////////////////////////////////////
+
+
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "Servicio Cantify",
+  });
+});
+
+
+// Start the server with selected configuration [5]
+////////////////////////////////////////////////////////////////////////
+
+mongoose
+  .connect(dbConfig.url, dbConfig.options)
+  .then(() => {
+    console.log("Conexion a base de datos: exitosa");
+  })
+  .catch((err) => {
+    console.log("Conexion a base de datos: falló: " + err);
+    process.exit();
+  });
+
+app.listen(port, () => {
+  console.log("Server is listening on port " + port);
+});
