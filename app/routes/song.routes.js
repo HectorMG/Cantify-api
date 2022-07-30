19c5f5c49f45d140fb8cffa842af7d0185f11dd2
ahhernandez <ahhernandez@indracompany.com>
2022-07-30 15:08:36 -0500

    Se modifica ruta para los endpoints de canciones

--- app/routes/song.routes.js
+++ app/routes/song.routes.js
@@ -4,7 +4,7 @@ module.exports = (app) => {
     const songs = require('../controllers/song.controller.js');
 
     // List all Songs
-    app.get('/api/canciones', auth, songs.findAll);
+    app.get('/api/songs', auth, songs.findAll);
     // Get a single Song by id
-    app.get('/api/canciones/:id', auth, songs.findOne);
+    app.get('/api/songs/:id', auth, songs.findOne);
 }
