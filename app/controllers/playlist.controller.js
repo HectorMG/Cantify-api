90f4e5a923a56ad5f07c10e647ae8c258f670c04
HectorMG <hector980715@gmail.com>
2022-07-30 10:33:46 -0500

    Se crean endpoints para listar todas las playlists y ver consultar una en especifico

--- app/controllers/playlist.controller.js
+++ app/controllers/playlist.controller.js
@@ -1,29 +1,63 @@
 const Playlist = require("../models/playlist.model");
 
-exports.create = (req,res) =>{
+exports.create = (req, res) => {
 
 
-  if (Object.keys(req.body).length === 0) {
-    return res.status(400).send({
-      message: "Playlist data can not be empty",
+    if (Object.keys(req.body).length === 0) {
+        return res.status(400).send({
+            message: "Playlist data can not be empty",
+        });
+    }
+
+    const playlist = new Playlist({
+        name: req.body.name,
+        description: req.body.description || null
     });
-  }
-
-  const playlist = new Playlist({
-    name: req.body.name,
-    description: req.body.description || null
-  });
 
 
-  playlist
-    .save()
-    .then((data) => {
-      res.status(200).send(data);
-    })
-    .catch((err) => {
-      res.status(500).send({
-        message:
-          err.message || "Something wrong occurred while creating the record.",
-      });
+    playlist
+        .save()
+        .then((data) => {
+            res.status(200).send(data);
+        })
+        .catch((err) => {
+            res.status(500).send({
+                message:
+                    err.message || "Something wrong occurred while creating the record.",
+            });
+        });
+}
+
+
+exports.findAll = (req, res) => {
+    Playlist.find()
+        .then(playlists => {
+            res.status(200).send(playlists);
+        }).catch(err => {
+            res.status(500).send({
+                message: err.message || "Something wrong occurred while retrieving the records."
+            });
+        });
+}
+
+exports.findOne = (req,res) =>{
+    Playlist.findById(req.params.id).then(playlist => {
+        if (!playlist) {
+            return res.status(404).send({
+                message: "Playlist not found with id:" + req.params.id
+            });
+        }
+        res.status(200).send(playlist);
+    }).catch(err => {
+        if (err.kind === 'ObjectId') {
+            return res.status(404).send({
+                message: "Playlist not found with id:" + req.params.id
+            });
+        }
+        return res.status(500).send({
+            message: "Something wrong ocurred while retrieving the record with id:"
+                + req.params.id
+        });
     });
+
 }
--- app/routes/playlist.routes.js
+++ app/routes/playlist.routes.js
@@ -1,7 +1,14 @@
 module.exports = (app) => {
     const playlist = require('../controllers/playlist.controller')
 
-    // validate Login
+    //Create a play list
     app.post('/api/playlist', playlist.create);
+
+    //list all playlist
+    app.get('/api/playlist',playlist.findAll);
+
+     // Get a single playlist by id
+     app.get('/api/playlist/:id',playlist.findOne);
+
 
 }
